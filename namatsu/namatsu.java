c185d67602cef5180ba2c3acab79aa952d6c241b
404-AgungSetyo-MAS <404-AgungSetyo-MAS@users.noreply.github.com>
2020-09-16 11:17:23 +0700

    namatsu.java

---
+++ namatsu/namatsu.java
@@ -0,0 +1,8 @@
+public class namatsu {
+
+	public static void main(String[] args) {
+		System.out.println("Nama: AgungMAS");
+		System.out.println("NIM: 6706202082");
+		System.out.println("Kelas: D3IF44-02");
+	}
+}
